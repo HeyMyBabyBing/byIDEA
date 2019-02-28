faee898414211d0354e582d84058357fba729fdf
IT_Monster <2211464277@qq.com>
2019-02-28 15:28:31 +0800

    1、修改了错误 2、给字段新增了setter/getter

--- src/main/java/com/dayao/pojo/User.java
+++ src/main/java/com/dayao/pojo/User.java
@@ -9,6 +9,29 @@ package com.dayao.pojo;
 public class User {
     private String name;
     private int age;
-    privte String sex;
+    private String sex;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
